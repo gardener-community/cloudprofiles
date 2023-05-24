96dc0f824585b0c7bfe64c2189799e401105acf3
Malte Münch <muench@23technologies.cloud>
2023-05-24 16:26:12 +0200

    Deprecates old versions automatically (instead of removing)
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/generate-k8s-versions/get-k8s-versions.py
+++ hack/generate-k8s-versions/get-k8s-versions.py
@@ -11,6 +11,8 @@ import sys
 import ruamel.yaml as yaml
 import os
 
+versions_pot = {}
+
 g = Github(os.getenv("GITHUB_TOKEN"))
 old_versions = []
 with open(sys.argv[1], "r") as stream:
@@ -18,20 +20,17 @@ with open(sys.argv[1], "r") as stream:
         values = yaml.safe_load(stream)
         for version in list(values["global"]["kubernetes"]["upstreamVersions"]["versions"].keys()):
             old_versions.append(semver.VersionInfo.parse(version))
+            versions_pot[version] = "deprecated"
     except yaml.YAMLError as exc:
         print(exc)
         exit(1)
-old_versions.sort() # newest version will be last in list
-
-if len(old_versions) != 4:
-    print("Error, expecting exactly 4 supported kubernetes versions.")
-    exit(1)
+old_versions.sort() # newest version will be last in list (index -1)
 
 
-minor_old0 = semver.VersionInfo(1, old_versions[3].minor - 0, 0, "incomplete")
-minor_old1 = semver.VersionInfo(1, old_versions[3].minor - 1, 0, "incomplete")
-minor_old2 = semver.VersionInfo(1, old_versions[3].minor - 2, 0, "incomplete")
-minor_old3 = semver.VersionInfo(1, old_versions[3].minor - 3, 0, "incomplete")
+minor_old0 = semver.VersionInfo(1, old_versions[-1].minor - 0, 0, "incomplete")
+minor_old1 = semver.VersionInfo(1, old_versions[-1].minor - 1, 0, "incomplete")
+minor_old2 = semver.VersionInfo(1, old_versions[-1].minor - 2, 0, "incomplete")
+minor_old3 = semver.VersionInfo(1, old_versions[-1].minor - 3, 0, "incomplete")
 
 new_versions = []
 
@@ -69,26 +68,18 @@ if len(new_versions) != 4:
     print("Error, expecting exactly 4 supported kubernetes versions.")
     exit(1)
 
-new_versions.sort() # newest version will be last in list
-
 # Version step is 0 if no version bump happens at all, it is 1 if only patch versions increase
 # it is 2 if at least one minor version bump took place
 version_step = 0
 
-
 # Assuming major is always 1:
 # lets check if a patch update took place
 for newversion in new_versions:
-    for oldversion in old_versions:
-        if oldversion.minor == newversion.minor:
-            if newversion.patch > oldversion.patch:
-                version_step = 1 # only patch updates so far
-                print("patch update: ", str(oldversion), "->", str(newversion))
+    if str(newversion) not in versions_pot:
+        print("patch update: ", str(newversion))
+        version_step = 1
+    versions_pot[str(newversion)] = "supported"
 
-# check if the minor version has been upgraded
-if new_versions[3].minor > old_versions[3].minor:
-    version_step = 2 # minor update as well, override patch update version bump
-    print("minor update: ", str(old_versions[3]), "->", str(new_versions[3]))
 
 if version_step == 0:
     # All done
@@ -99,8 +90,8 @@ with open(sys.argv[1], "r") as stream:
     try:
         values = yaml.safe_load(stream)
         values["global"]["kubernetes"]["upstreamVersions"]["versions"].clear()
-        for version in new_versions:
-            values["global"]["kubernetes"]["upstreamVersions"]["versions"][str(version)] = {"classification": "supported"}
+        for version in versions_pot:
+            values["global"]["kubernetes"]["upstreamVersions"]["versions"][version] = {"classification": versions_pot[version]}
         with open(sys.argv[1], "w") as fp:
             try:
                 yaml.dump(values, fp, default_flow_style=False)
@@ -118,8 +109,6 @@ with open(sys.argv[2], "r") as stream:
         chart_version = semver.VersionInfo.parse(chart_yaml["version"])
         if version_step == 1:
             chart_version = chart_version.bump_patch()
-        elif version_step == 2:
-            chart_version = chart_version.bump_minor()
         chart_yaml["version"] = str(chart_version)
         with open(sys.argv[2], "w") as fp:
             try:
